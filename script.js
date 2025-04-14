76fd614cd382baa0a5dd687e25b9755c32cd02a0
agrawalkavya <123631397+agrawalkavya@users.noreply.github.com>
2025-04-14 12:25:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const foodData = {
-  "Canteen A": ["Chole Bhature", "Samosa", "Aloo Paratha", "Pasta", "Momos"],
-  "Canteen B": ["Burger", "Pizza", "French Fries", "Cold Coffee"],
-  "Canteen C": ["Maggi", "Paneer Roll", "Sandwich", "Tea"],
-  "Canteen D": ["Rajma Chawal", "Lassi", "Spring Roll"],
-  "Canteen E": ["Idli", "Dosa", "Vada", "Sambar"],
-  "Canteen F": ["Thali", "Noodles", "Lemonade"],
-  "Canteen G": ["Pav Bhaji", "Pani Puri", "Chai"],
-  "Canteen H": ["Tacos", "Ice Cream", "Wrap", "Milkshake"]
+  "Udupi": ["Tea", "Hot coffee", "Cold coffee", "Banana shake", "Mango shake", "Oreo shake", "Lassi", "Cold drinks", "Ice cream", "Samosa", "Bread Pakoda", "Veg Sandwich", "Burger", "Grilled Sandwich", "Maggi", "Chole bhature", "Aloo Puri", "Vada Sambar", "Idli Sambar", "Masala dosa", "Paneer Dosa", "Plain Dosa", "Chole chawal", "Rajma Chawal", "Uttapam", "Pav bhaji", "Chowmein", "Spring roll", "Chilli potato", "Chilli paneer", "French fries", "Manchurian", "Aloo Paratha", "Aloo Pyaz", "Paneer Paratha"],
+  "Bistro": ["Burger", "Pizza", "French Fries", "Cold Coffee"],
+  "HIMS": ["Maggi", "Paneer Roll", "Sandwich", "Tea"],
+  "DelTech Cafe": ["Rajma Chawal", "Lassi", "Spring Roll"],
+  "HPMC": ["Idli", "Dosa", "Vada", "Sambar"],
+  "Amul": ["Thali", "Noodles", "Lemonade"],
+  "Dosa Plaza": ["Pav Bhaji", "Pani Puri", "Chai"],
+  "Dazzledine": ["Tacos", "Ice Cream", "Wrap", "Milkshake"],
+  "KitKat Zone": ["Tacos", "Ice Cream", "Wrap", "Milkshake"],
+  "Nescafe": ["Tacos", "Ice Cream", "Wrap", "Milkshake"],
+  "Raydee Cafe": ["Tacos", "Ice Cream"]
 };
 
 function getRandomFood() {
